16e14847e934ab2348b2254d76697e5ec6c52afd
junstar21 <115910833+junstar21@users.noreply.github.com>
2022-10-16 14:03:11 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write('# Youtube view')
+st.write("## raw")
+
+view = [100, 150, 30]
+view
+st.write('## bar chart')
+st.bar_chart(view)
+
+import pandas as pd
+sview = pd.Series(view)
+sview
